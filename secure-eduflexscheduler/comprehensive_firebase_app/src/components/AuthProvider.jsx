606762c669ef75368e81c06c9541541838062711
Sistersocrates <danielle.staley3@gmail.com>
2025-08-14 21:34:00 -0500

    Update AuthProvider.jsx

--- secure-eduflexscheduler/comprehensive_firebase_app/src/components/AuthProvider.jsx
+++ secure-eduflexscheduler/comprehensive_firebase_app/src/components/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth, getUserProfile } from '../lib/firebase';
+import { auth, getUserProfile } from '../lib/firebase_enhanced';
 
 const AuthContext = createContext({});
 
